a092f0dca0964eada7138d9868ceef5d48540ddf
SaraiBenjamins <152277895+SaraiBenjamins@users.noreply.github.com>
2024-11-29 09:36:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+var eersteStreepjesButton = document.querySelector("section.streepjes li:nth-of-type(1) button");
+eersteStreepjesButton.onclick = eersteStreepjesButtonKlik;
+
+function eersteStreepjesButtonKlik() {
+  // Haal de waarde van aria-expanded op met getAttribute
+  var ariaExpanded = eersteStreepjesButton.getAttribute("aria-expanded");
+
+  if (ariaExpanded === "false") {
+    // Zet aria-expanded naar true met setAttribute
+    eersteStreepjesButton.setAttribute("aria-expanded", "true");
+  } else {
+    // Zet aria-expanded naar false met setAttribute
+    eersteStreepjesButton.setAttribute("aria-expanded", "false");
+  }
+}
